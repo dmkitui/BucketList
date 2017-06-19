9d24587b06a0ea3bbb698c4e87292f4e54697988
dmkitui <daniel.kitui@andela.com>
2017-06-19 08:39:08 +0300

    [ft #147095419] Refactor tests to pass weak password validation

--- tests/test_users.py
+++ tests/test_users.py
@@ -11,23 +11,29 @@ class UsersModelTestCase(BaseTestCase):
 
     def test_valid_user_registration(self):
         '''Test for a user registration'''
-        response = self.user_registration('user@gmail.com', 'password', 'password')
+        response = self.user_registration('user@gmail.com', 'Password01', 'Password01')
         self.assertEqual(response.status_code, 201)
         self.assertIn(b'Registration successful, welcome to Bucketlist', response.data)
 
     def test_invalid_registration_wrong_passwords_match(self):
         '''Tests a wrong password match'''
-        response = self.user_registration('user@gmail.com', 'password', 'PASSWORD')
+        response = self.user_registration('user2@gmail.com', 'Password01', 'PASSWORD01')
         self.assertEqual(response.status_code, 401)
         self.assertIn(b'Password fields do not match', response.data)
 
+    def test_user_registration_weak_password(self):
+        '''Test for registartion using weak password'''
+        response = self.user_registration('user010@example.com', 'password', 'password')
+        self.assertEqual(response.status_code, 401)
+        self.assertIn(b'Weak password. Make sure password contains at least 8 characters, an uppercase letter, and a digit', response.data)
+
     def test_duplicate_user_registration(self):
         '''Tests to prevent an already registred email being used again'''
-        response = self.user_registration('dan@gmail.com', 'password', 'password')
+        response = self.user_registration('dan@gmail.com', 'Password01', 'Password01')
         self.assertEqual(response.status_code, 201)
 
         #Register using same email address again
-        response = self.user_registration('dan@gmail.com', 'password', 'password')
+        response = self.user_registration('dan@gmail.com', 'passworD02', 'passworD02')
         self.assertEqual(response.status_code, 202)     # The request was received, but wont be acted uopn
         self.assertIn(b'Registration Failure. User dan@gmail.com already registered', response.data)
 
@@ -40,17 +46,17 @@ class UsersModelTestCase(BaseTestCase):
     def test_user_login_valid_credentials(self):
         '''test for a successful user login'''
 
-        response = self.user_registration('newuser@gmail.com', '123456', '123456')  # Create new user
+        response = self.user_registration('newuser@gmail.com', 'Qwerty03', 'Qwerty03')  # Create new user
         self.assertEqual(response.status_code, 201)
 
         # Login
-        response = self.user_login('newuser@gmail.com', '123456')
+        response = self.user_login('newuser@gmail.com', 'Qwerty03')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Login successful', response.data)
 
     def test_user_login_invalid_password(self):
         '''Test for login with invalid password'''
-        response = self.user_registration('newuser1@gmail.com', '123456', '123456')  # Create new user
+        response = self.user_registration('newuser1@gmail.com', 'Qwerty04', 'Qwerty04')  # Create new user
         self.assertEqual(response.status_code, 201)
 
         # Login with invalid credentials
